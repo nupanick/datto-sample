c935500384ae10c901c533141fc7541dcd0f59b8
Nicholas Lamicela <nupanick@gmail.com>
2015-09-04 12:27:29 -0400

    Merged sanitize() with repeatIndex().

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Find the first word in a document with the maximum number of repetitions of
 # any given letter.
 
-from string import ascii_lowercase as whitelist     # For avoiding punctuation
+import string   # For building the whitelist.
 
 teststr = "O Romeo, Romeo, wherefore art thou Romeo?"
 
@@ -13,23 +13,15 @@ teststr = "O Romeo, Romeo, wherefore art thou Romeo?"
 
 #for word in teststr.split(" ");
 
-def sanitize(word):
-    """ Convert letters to lowercase, and remove everything else. """
-    word = str.lower(word)
-    cleanword = ''
-    for ch in word:
-        if ch in whitelist:
-            cleanword += ch
-    return cleanword
-
 def repeatIndex(word):
     """ Return the number of times the most common letter in the word appears
         in the word. """
-    word = sanitize(word)   # Ignore capitalization and punctuation
-    skip = []               # Track already counted letters
+    word = word.lower()                 # Ignore capitalization.
+    whitelist = string.ascii_lowercase  # Ignore punctuation.
+    skip = []                           # Track already counted letters.
     bestScore = 0
     for ch in word:
-        if ch not in skip:
+        if ch not in skip and ch in whitelist:
             skip += ch
             score = word.count(ch)
             if score > bestScore:
