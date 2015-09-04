a1127a387e1cff09db62c28459291c0d3f2b662b
Nicholas Lamicela <nupanick@gmail.com>
2015-09-04 12:59:54 -0400

    Added file i/o logic.

--- main.py
+++ main.py
@@ -4,10 +4,36 @@
 # Find the first word in a document with the maximum number of repetitions of
 # any given letter.
 
+import sys      # For the command line argument.
 import string   # For building the whitelist.
 
 def main():
-    pass
+    """ Read in a text file and print out the word with the highest
+        repeatIndex() score. """
+    
+    # Get filename from args
+    if not len(sys.argv) == 2:
+        print("Usage: " + sys[0] + "<input file>")
+        return
+    filename = sys.argv[1]
+
+    # Read file line by line, word by word, looking for the best score.
+    bestWord = ""
+    bestScore = 0
+
+    # Open file.
+    with open(filename) as f:
+        # Read line.
+        for line in f.readlines():
+            # Read word.
+            for word in line.split(" "):
+                # Word-by-word logic goes here.
+                
+                print(word)
+
+            # Done reading word.
+        # Done reading line.
+    # Close file.
 
 def repeatIndex(word):
     """ Return the number of times the most common letter in the word appears
@@ -32,5 +58,5 @@ for word in teststr.split(" "):
 """
 
 # (main trigger)
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
